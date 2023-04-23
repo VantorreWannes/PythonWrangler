81e438209bc07b31525d5f7216236577f439e5e8
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-04-23 11:19:23 +0200

    Fix test_all() settings

--- V1/TestDecorators.py
+++ V1/TestDecorators.py
@@ -1,6 +1,7 @@
 import functools
 from AffirmError import AffirmIsFalse
 
+
 def test(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -13,31 +14,34 @@ def test(func):
                 print(f"|ER|: {func.__name__}")
             if wrapper.crash_on_false:
                 err.raise_to_level(2)
-            else:
-                pass
     wrapper.crash_on_false: bool = True
     wrapper.verbose: bool = True
     wrapper.test = True
     return wrapper
 
+
 def get_all_test_functions(cls):
-    return [method for name, method in cls.__dict__.items() if hasattr(method, 'test')]
+    return [method for _, method in cls.__dict__.items() if hasattr(method, 'test')]
+
 
 def test_all(cls, crash_on_false=True, verbose=True):
     for test_func in get_all_test_functions(cls):
-        setattr(test_func, "crash_on_false", crash_on_false)
-        setattr(test_func, "verbose", verbose)
+        setattr(test_func, "verbose", False)
         try:
             test_func(cls())
+            if verbose:
+                print(f"|OK|: {cls.__name__}::{test_func.__name__}")
         except AffirmIsFalse as err:
-            if test_func.crash_on_false:
+            if verbose:
+                print(f"|ER|: {cls.__name__}::{test_func.__name__}")
+            if crash_on_false:
                 err.raise_to_level(2)
-            else:
-                pass
+
 
 def test_class(cls):
     cls.test_all = classmethod(test_all)
     return cls
+
 
 if __name__ == "__main__":
     print("Ran")
--- V1/Tests/Class.py
+++ V1/Tests/Class.py
@@ -1,19 +1,21 @@
-import sys
-sys.path.append(r'C:\Users\Wannes\Desktop\JokNavi\2023\Coding\Python\PythonWrangler\V1')
-from Affirms import affirm, affirm_eq, affirm_ne
 from TestDecorators import test, test_class
+from Affirms import affirm, affirm_eq, affirm_ne
+import sys
+sys.path.append(
+    r'C:\Users\Wannes\Desktop\JokNavi\2023\Coding\Python\PythonWrangler\V1')
 
 
 class StupidClass:
 
     def add(self, left, right):
         return left + right
-    
+
     def append(self, text, string):
         return text + string
 
+
 @test_class
-class TestStupidClass: 
+class TestStupidClass:
 
     @test
     def test_add(self):
@@ -21,12 +23,14 @@ class TestStupidClass:
         add_result = SC.add(1, 5)
         affirm_eq(add_result, 6)
 
-
     @test
     def test_append(self):
         SC = StupidClass()
         append_result = SC.append("Hi", " i'm nav!")
         affirm_eq(append_result, "Hi i'm nav")
+
+    def test_print(self):
+        print("this won't print")
 
 
 if __name__ == "__main__":
