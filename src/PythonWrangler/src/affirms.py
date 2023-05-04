02789824086db8945a346dd035a2405941e7d444
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-05-04 09:32:32 +0200

    Add raises() function
    - Add raises function
    - Rename affirm_error.py

--- src/PythonWrangler/src/affirms.py
+++ src/PythonWrangler/src/affirms.py
@@ -1,11 +1,28 @@
-class RaisedWrongType(Exception):
 
-    def __init__(self, actual_exception_type: BaseException, expected_exception_type: BaseException) -> None:
-        self.message = f"Expression raised: {IndexError().__class__.__name__}, instead of expected: {TabError().__class__.__name__}."
-        super().__init__(self.message)
+def raises(function: object, expected_exception: Exception, *args, **kwargs):
+    """
+    Checks if the raised error from the first parameter is equal to the expected exception.
+    Checks for type if expected_exception is a type.
+    Checks for equality if expected_exception is initiated.
+    """
+    try:
+        function(*args, **kwargs)
+    except BaseException as e:
+        if isinstance(expected_exception, type):
+            return isinstance(e, expected_exception)
+        else:
+            return isinstance(e, type(expected_exception)) and e.args == expected_exception.args
 
+def is_true(contition: bool):
+    if not isinstance(contition, bool):
+        raise TypeError("Test condition should be of type: bool")
+
+def test():
+    raise IndexError("lol")
 
 if __name__ == "__main__":
-    error = RaisedWrongType(type(IndexError()), type(TabError()))
-    raise error
-    
+    print(raises(test, IndexError)) # True
+    print(raises(test, IndexError("lol"))) # True
+    print(raises(test, IndexError())) # False
+    print(raises(test, IndexError("XD"))) # False
+
