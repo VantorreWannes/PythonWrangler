abce085958b08ad07c55718e4f7b3b0b8250ded7
Navoso <VantorreWannes@gmail.com>
2024-03-25 19:03:53 +0100

    update version

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -5,9 +5,9 @@ def test_affirm_implicit():
     affirm(1 == 1)
 
 @test(crash_on_false=False, verbose=True)
-def test_affirm_full():
+def test_affirm_explicit():
     affirm(0 == 1)
 
 if __name__ == '__main__':
     test_affirm_implicit()
-    test_affirm_full()
+    test_affirm_explicit()
